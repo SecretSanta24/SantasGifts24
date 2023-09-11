66711bbcc6675f188e0d9e34926f9903ee3c9bde
l-Luna <izzeldeen03@gmail.com>
2023-09-11 15:27:46 +0100

    minor cleaning

--- SantasGifts24Module.cs
+++ SantasGifts24Module.cs
@@ -1,33 +1,33 @@
 ﻿using System;
 using Microsoft.Xna.Framework;
 
-namespace Celeste.Mod.SantasGifts24 {
-    public class SantasGifts24Module : EverestModule {
-        public static SantasGifts24Module Instance { get; private set; }
+namespace Celeste.Mod.SantasGifts24;
 
-        public override Type SettingsType => typeof(SantasGifts24ModuleSettings);
-        public static SantasGifts24ModuleSettings Settings => (SantasGifts24ModuleSettings) Instance._Settings;
+public class SantasGifts24Module : EverestModule {
+	public static SantasGifts24Module Instance { get; private set; }
 
-        public override Type SessionType => typeof(SantasGifts24ModuleSession);
-        public static SantasGifts24ModuleSession Session => (SantasGifts24ModuleSession) Instance._Session;
+	public override Type SettingsType => typeof(SantasGifts24ModuleSettings);
+	public static SantasGifts24ModuleSettings Settings => (SantasGifts24ModuleSettings) Instance._Settings;
 
-        public SantasGifts24Module() {
-            Instance = this;
+	public override Type SessionType => typeof(SantasGifts24ModuleSession);
+	public static SantasGifts24ModuleSession Session => (SantasGifts24ModuleSession) Instance._Session;
+
+	public SantasGifts24Module() {
+		Instance = this;
 #if DEBUG
-            // debug builds use verbose logging
-            Logger.SetLogLevel(nameof(SantasGifts24Module), LogLevel.Verbose);
+		// debug builds use verbose logging
+		Logger.SetLogLevel(nameof(SantasGifts24Module), LogLevel.Verbose);
 #else
             // release builds use info logging to reduce spam in log files
             Logger.SetLogLevel(nameof(SantasGifts24Module), LogLevel.Info);
 #endif
-        }
+	}
 
-        public override void Load() {
-            // TODO: apply any hooks that should always be active
-        }
+	public override void Load() {
+            
+	}
 
-        public override void Unload() {
-            // TODO: unapply any hooks applied in Load()
-        }
-    }
+	public override void Unload() {
+            
+	}
 }
