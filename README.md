e9e72e448b39583357e8b67b92ac40df09a3a390
l-Luna <izzeldeen03@gmail.com>
2023-09-11 15:27:26 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Santa's Gifts 2024
+
+The code mod for Secret Santa Collab 2024.
---
+++ SantasGifts24ModuleSettings.cs
@@ -0,0 +1,5 @@
+namespace Celeste.Mod.SantasGifts24 {
+    public class SantasGifts24ModuleSettings : EverestModuleSettings {
+
+    }
+}
