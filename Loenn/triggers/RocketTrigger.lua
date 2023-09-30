5e8e05af3caeef6217ce1e23c69e448b556542ee
Aurora Aquir <AuroraKy@users.noreply.github.com>
2023-09-30 20:10:11 +0200

    Add RocketState and RocketTrigger and coding art :D

---
+++ Loenn/triggers/RocketTrigger.lua
@@ -0,0 +1,14 @@
+﻿local trigger = {}
+
+trigger.name = "SS2024/RocketTrigger"
+trigger.placements = {
+    name = "RocketTrigger",
+    data = {
+        enable = true,
+        maxSpeed = 250.0,
+        timeToMaxSpeed = 0.25,
+        timeToStop = 0.25,
+    }
+}
+
+return trigger
