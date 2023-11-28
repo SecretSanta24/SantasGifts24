bc6b8250a6e3cc8fa6153cf54f7c1371c0b09270
Aurora Aquir <AuroraKy@users.noreply.github.com>
2023-11-28 14:50:30 +0100

    Fix RWC not detecting duplicates + image

--- Loenn/entities/RewindController.lua
+++ Loenn/entities/RewindController.lua
@@ -1,8 +1,7 @@
-local drawableRectangle = require("structs.drawable_rectangle")
-local utils = require("utils")
 local entity = {}
 
 entity.name = "SS2024/RewindController"
+entity.texture = "objects/controllers/rwc/RWController"
 entity.placements = {
     {
         name = "RewindController",
@@ -14,15 +13,6 @@ entity.placements = {
     }
 }
 
-function entity.sprite(room, entity, viewport)
-    local rectangle
-    local drawableSprite
-    local fillColor = {0, 0, 1}
-    rectangle = utils.rectangle(entity.x, entity.y, 8, 8)
-    drawableSprite = drawableRectangle.fromRectangle("fill", rectangle, fillColor)
-    drawableSprite.depth = entity.depth
-    return drawableSprite
-end
 
 return nil
 -- return entity
