0172f6b6368647c79ca311799a5716101f1e6ec8
vitellaryjr <43586575+vitellaryjr@users.noreply.github.com>
2023-11-28 17:14:13 -0700

    WC blocker

---
+++ Code/Triggers/MonoBlocker.cs
@@ -0,0 +1,23 @@
+﻿using Celeste.Mod.Entities;
+using Microsoft.Xna.Framework;
+using Monocle;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Celeste.Mod.SantasGifts24.Code.Triggers
+{
+    [TrackedAs(typeof(LookoutBlocker))]
+    [Tracked(false)]
+    [CustomEntity("SS2024/MonoBlocker")]
+    public class MonoBlocker : LookoutBlocker
+    {
+        public string Flag;
+        public MonoBlocker(EntityData data, Vector2 offset) : base(data, offset)
+        {
+            Flag = data.Attr("flag", "");
+        }
+    }
+}
