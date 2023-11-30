8e0f0f0da33dd16bc9157579a3b8bc9d1c4f4c16
ricky0606 <98306742+ricky0606@users.noreply.github.com>
2023-11-30 02:21:27 -0800

    add: lightning

---
+++ Code/Entities/FlagLightning.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Celeste;
+using Microsoft.Xna.Framework;
+using Monocle;
+using Celeste.Mod.Entities;
+
+namespace Celeste.Mod.SantasGifts24.Code.Entities
+{
+    [CustomEntity("SS2024/FlagLightning")]
+    public class FlagLightning : Lightning
+    {
+        private bool previousFlagState;
+        public FlagLightning(Vector2 position, int width, int height, Vector2? node, float moveTime)
+            : base(position, width, height, node, moveTime)
+        {
+            VisualWidth = width;
+            VisualHeight = height;
+            base.Collider = new Hitbox(width - 2, height - 2, 1f, 1f);
+            Add(new PlayerCollider(OnPlayer));
+            if (node.HasValue)
+            {
+                Add(new Coroutine(MoveRoutine(position, node.Value, moveTime)));
+            }
+            toggleOffset = Calc.Random.NextFloat();
+        }
+
+        public FlagLightning(EntityData data, Vector2 levelOffset)
+            : this(data.Position + levelOffset, data.Width, data.Height, data.FirstNodeNullable(levelOffset), data.Float("moveTime"))
+        {
+        }
+
+        public override void Update()
+        {
+            base.Update();
+            // flag on = electric off 
+            bool currentFlagState = SceneAs<Level>().Session.GetFlag("SS2024_level_electricity_flag");
+
+            if (currentFlagState)
+            {
+                Collidable = false;
+                Visible = false;
+                if (currentFlagState != previousFlagState)
+                {
+                    Scene.Tracker.GetEntity<LightningRenderer>().Untrack(this);
+                }
+            }
+            else
+            {
+                Collidable = true;
+                Visible = true;
+                if (currentFlagState != previousFlagState)
+                {
+                    Scene.Tracker.GetEntity<LightningRenderer>().Track(this);
+                }
+            }
+                previousFlagState = currentFlagState;
+        }
+    }
+
+}
---
+++ Loenn/entities/flagLightning.lua
@@ -0,0 +1,18 @@
+local xnaColors = require("consts.xna_colors")
+local yellow = xnaColors.Yellow
+local flagLightning = {}
+
+flagLightning.name = "SS2024/FlagLightning"
+flagLightning.fillColor = {yellow[1] * 0.3, yellow[2] * 0.3, yellow[3] * 0.3, 0.6}
+flagLightning.borderColor = {yellow[1] * 0.8, yellow[2] * 0.8, yellow[3] * 0.8, 0.8}
+flagLightning.placements = {
+    name = "Flag Lightning",
+    data = {
+        width = 8,
+        height = 8
+    }
+}
+
+flagLightning.depth = -1000000
+
+return nil
