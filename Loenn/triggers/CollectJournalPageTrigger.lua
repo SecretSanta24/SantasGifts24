12450d8e8f7993dd2a0b234163bfe95927bf62e8
Aurora Aquir <AuroraKy@users.noreply.github.com>
2023-12-10 23:14:57 +0100

    integrate deers controller so they can use it + random controller image

---
+++ Loenn/triggers/CollectJournalPageTrigger.lua
@@ -0,0 +1,12 @@
+﻿local trigger = {}
+
+trigger.name = "SS2024/CollectJournalPageTrigger"
+trigger.placements = {
+    name = "CollectJournalPageTrigger",
+    data = {
+        image = "",
+    }
+}
+
+--return trigger
+return nil
