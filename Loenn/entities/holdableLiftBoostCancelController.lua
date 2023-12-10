a7283b4c48e767a8fea5fc0ab435da9aeb596a0a
Aurora Aquir <AuroraKy@users.noreply.github.com>
2023-12-10 22:12:44 +0100

    Add a thing snip made into the helper

---
+++ Loenn/entities/holdableLiftBoostCancelController.lua
@@ -0,0 +1,12 @@
+local holdableLiftBoostCancelController = {}
+
+holdableLiftBoostCancelController.name = "LiftBoostCancel/HoldableLiftBoostCancelController"
+holdableLiftBoostCancelController.texture = "objects/controllers/LiftBoostCancel/holdableLiftBoostCancelController"
+
+holdableLiftBoostCancelController.placements = {
+  name = "controller",
+  data = {}
+}
+
+-- return holdableLiftBoostCancelController
+return nil
