9cf6824f5ffa1c2d76ee267ca7082a6a73769de0
corkr900 <109830893+corkr900@users.noreply.github.com>
2023-12-20 22:49:09 -0600

    Animate the bouncy dude and add freeze frames to explosion knockback

--- Loenn/entities/BouncyDude.lua
+++ Loenn/entities/BouncyDude.lua
@@ -2,7 +2,7 @@ local bouncyDude = {}
 
 bouncyDude.name = "SS2024/BouncyDude"
 bouncyDude.depth = -2000000
-bouncyDude.texture = "objects/ss2024/bouncyDude/normal00"
+bouncyDude.texture = "objects/ss2024/bouncyDude/idle02"
 bouncyDude.offset = { 12, 24 }
 bouncyDude.placements = {
     name = "bouncyDude"
