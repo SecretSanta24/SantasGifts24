41c9dd96255d177828f3b5df88d4754799611c7b
ricky0606 <98306742+ricky0606@users.noreply.github.com>
2023-12-23 17:49:42 -0800

    bugfixes

---
+++ Loenn/entities/electricDoor.lua
@@ -0,0 +1,13 @@
+local electricDoor = {}
+
+electricDoor.name = "SS2024/ElectricDoor"
+electricDoor.placements = {
+    name = "Electric Door",
+    data = {
+        right = false
+    }
+}
+
+electricDoor.texture = "objects/SS2024/electricity/door_close00"
+
+return nil
