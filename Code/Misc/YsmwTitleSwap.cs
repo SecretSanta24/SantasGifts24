68cf766e923824dca9a58e448bd7c35f11080556
l-Luna <izzeldeen03@gmail.com>
2024-07-23 22:39:35 +0100

    Hidden in the foliage lies a tattered looking map title...

--- Code/Misc/YsmwTitleSwap.cs
+++ Code/Misc/YsmwTitleSwap.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Microsoft.Xna.Framework;
 using Mono.Cecil.Cil;
 using MonoMod.Cil;
 using MonoMod.RuntimeDetour;
@@ -11,10 +12,14 @@ public static class YsmwTitleSwap{
         using(var _ = new DetourContext(-1, "YsmwTitleSwap")){
             IL.Celeste.OuiChapterPanel.Render += ModChapterPanelRender;
         }
+        
+        // now let's do a funny one!
+        On.Celeste.OuiChapterPanel.Render += HookChapterPanelRender;
     }
 
     public static void Unload(){
         IL.Celeste.OuiChapterPanel.Render -= ModChapterPanelRender;
+        On.Celeste.OuiChapterPanel.Render -= HookChapterPanelRender;
     }
     
     private static void ModChapterPanelRender(ILContext il){
@@ -28,5 +33,23 @@ public static class YsmwTitleSwap{
                 return oldName + suffix;
             return oldName;
         });
+    }
+    
+    private static void HookChapterPanelRender(On.Celeste.OuiChapterPanel.orig_Render orig, OuiChapterPanel self){
+        if(self.Data.SID == "SecretSanta2024/1-Easy/leppa" && !self.RealStats.Modes[0].Completed){
+            AreaData data = self.Data;
+            Color oldTitleBase = data.TitleBaseColor, oldTitleAccent = data.TitleAccentColor, oldTitleText = data.TitleTextColor;
+            try{
+                data.TitleBaseColor = Color.White;
+                data.TitleAccentColor = Color.Gray;
+                data.TitleTextColor = Color.Black;
+                orig(self);
+            }finally{
+                data.TitleBaseColor = oldTitleBase;
+                data.TitleAccentColor = oldTitleAccent;
+                data.TitleTextColor = oldTitleText;
+            }
+        }else
+            orig(self);
     }
 }
